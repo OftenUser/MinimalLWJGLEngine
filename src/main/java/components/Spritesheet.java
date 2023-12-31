8cd609264253c52fcba8ffb68f05fa4c18d43e96
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-12-31 15:21:31 -0500

    Update Spritesheet.java

--- src/main/java/components/Spritesheet.java
+++ src/main/java/components/Spritesheet.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Spritesheet {
-
     private Texture texture;
     private List<Sprite> sprites;
 
@@ -15,13 +14,15 @@ public class Spritesheet {
         this.sprites = new ArrayList<>();
 
         this.texture = texture;
+        
         int currentX = 0;
         int currentY = texture.getHeight() - spriteHeight;
-        for (int i=0; i < numSprites; i++) {
-            float topY = (currentY + spriteHeight) / (float)texture.getHeight();
-            float rightX = (currentX + spriteWidth) / (float)texture.getWidth();
-            float leftX = currentX / (float)texture.getWidth();
-            float bottomY = currentY / (float)texture.getHeight();
+        
+        for (int i = 0; i < numSprites; i++) {
+            float rightX = (currentX + spriteWidth) / (float) texture.getWidth();
+            float leftX = currentX / (float) texture.getWidth();
+            float topY = (currentY + spriteHeight) / (float) texture.getHeight();
+            float bottomY = currentY / (float) texture.getHeight();
 
             Vector2f[] texCoords = {
                     new Vector2f(rightX, topY),
@@ -29,10 +30,12 @@ public class Spritesheet {
                     new Vector2f(leftX, bottomY),
                     new Vector2f(leftX, topY)
             };
+            
             Sprite sprite = new Sprite(this.texture, texCoords);
             this.sprites.add(sprite);
 
             currentX += spriteWidth + spacing;
+            
             if (currentX >= texture.getWidth()) {
                 currentX = 0;
                 currentY -= spriteHeight + spacing;
