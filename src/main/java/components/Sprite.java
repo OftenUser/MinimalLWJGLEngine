7c61c8f616dfe7f74a338ec50adbe5cdd2b9b742
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-12-31 15:20:06 -0500

    Update Sprite.java

--- src/main/java/components/Sprite.java
+++ src/main/java/components/Sprite.java
@@ -4,18 +4,19 @@ import org.joml.Vector2f;
 import renderer.Texture;
 
 public class Sprite {
-
     private Texture texture;
     private Vector2f[] texCoords;
 
     public Sprite(Texture texture) {
         this.texture = texture;
+        
         Vector2f[] texCoords = {
                 new Vector2f(1, 1),
                 new Vector2f(1, 0),
                 new Vector2f(0, 0),
                 new Vector2f(0, 1)
         };
+        
         this.texCoords = texCoords;
     }
 
