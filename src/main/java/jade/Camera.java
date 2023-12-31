44f72cf0f609ec459b1eca83c5a14694eabee3d0
OftenGuy <77411288+OftenUser@users.noreply.github.com>
2023-12-31 15:15:13 -0500

    Update Camera.java

--- src/main/java/jade/Camera.java
+++ src/main/java/jade/Camera.java
@@ -12,6 +12,7 @@ public class Camera {
         this.position = position;
         this.projectionMatrix = new Matrix4f();
         this.viewMatrix = new Matrix4f();
+        
         adjustProjection();
     }
 
@@ -23,10 +24,10 @@ public class Camera {
     public Matrix4f getViewMatrix() {
         Vector3f cameraFront = new Vector3f(0.0f, 0.0f, -1.0f);
         Vector3f cameraUp = new Vector3f(0.0f, 1.0f, 0.0f);
+        
         this.viewMatrix.identity();
-        viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f),
-                                        cameraFront.add(position.x, position.y, 0.0f),
-                                        cameraUp);
+        
+        viewMatrix.lookAt(new Vector3f(position.x, position.y, 20.0f), cameraFront.add(position.x, position.y, 0.0f), cameraUp);
 
         return this.viewMatrix;
     }
